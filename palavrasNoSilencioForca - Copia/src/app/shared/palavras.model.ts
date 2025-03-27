3800e09e5609e585e66c3cebb4986614f66dd4aa
Bruno Ribeiro <56102954+mufasa-dev@users.noreply.github.com>
2025-03-27 00:12:06 -0300

    Delete palavrasNoSilencioForca - Copia directory

--- palavrasNoSilencioForca - Copia/src/app/shared/palavras.model.ts
+++
@@ -1,9 +0,0 @@
-export class Palavra{
-    public palavra: string;
-    public dica: string;
-
-    constructor(palavra: string, dica: string){
-        this.palavra = palavra;
-        this.dica = dica;
-    }
-}
